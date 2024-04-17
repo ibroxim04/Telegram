d187a911951d804113f9783f511280d90f1bcc39
Ibroxim <ibroximxamidov029@gmail.com>
2024-04-17 19:44:26 +0500

    Basic structure of the project was created Added AdminView and UserView classes and implemented singleton design pattern for each service classes.

---
+++ src/uz/pdp/ui/views/LoginView.java
@@ -0,0 +1,16 @@
+package uz.pdp.ui.views;
+
+import uz.pdp.backend.service.userService.UserService;
+import uz.pdp.backend.service.userService.UserServiceImp;
+
+public class LoginView {
+    private static UserService userService = UserServiceImp.getInstance();
+
+    public static void login() {
+
+    }
+
+    public static void signUp() {
+
+    }
+}
